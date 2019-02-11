e1ab12776606f50b30dc52b5e0327c2eefc1e7af
Dimitris Paraschakis <paraschakis@gmail.com>
2019-02-11 23:00:49 +0100

    Update restws.py

--- web_service/restws.py
+++ web_service/restws.py
@@ -1,21 +1,26 @@
-# sudo apt-get install python-pip
-# sudo pip install web.py
-# sudo pip install -U https://github.com/google/google-visualization-python/zipball/master
-# run as: http://localhost:8080/motionlog/<datetime_interval>
 import web
 from datetime import datetime
 from log_parser import LogParser
 
 urls = (
-    '/motionlog/(.*)', 'get_motionlog'
+    '/motionlog/(.*)', 'GetMotionLog'
 )
 
 app = web.application(urls, globals())
 
 class GetMotionLog:
+"""
+Creates a web service
+
+Setup:
+sudo apt-get install python-pip
+sudo pip install web.py
+sudo pip install -U https://github.com/google/google-visualization-python/zipball/master
+run as: http://localhost:8080/motionlog/<datetime_interval>
+"""
+
 	def GET(self, t_interval):
 		web.header('Access-Control-Allow-Origin', '*')
-		#web.header('Access-Control-Allow-Credentials', 'true')
 		url = 'http://192.168.20.251/axis-cgi/systemlog.cgi'
 		username = 'root'
 		password = 'pass'
@@ -24,4 +29,4 @@ class GetMotionLog:
 		return dt
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
